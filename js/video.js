af9d0cc8aba3f51c4e7dedfa41154b3170a3aa3e
shekhmohibur <mohibur.rahman2003@gmail.com>
2025-05-24 02:04:03 +0600

    removed unwanted js

--- js/video.js
+++ js/video.js
@@ -1,16 +1,5 @@
 const getCategoryBtn = async () => {
   const res = await fetch('https://openapi.programming-hero.com/api/phero-tube/categories');
   const data = await res.json();
-  categoryBtns(data);
-}
-
-getCategoryBtn();
-
-const categoryBtns = (btns) =>{
-    const cateWrap = document.getElementById('category-btn');
-    for(const btn of btns.categories){
-        console.log(btn)
-        const newBtn = document.createElement('button');
-        
-    }
+  categoryBtns(data.categories);
 }
